80b7f5ab05d3e1f75ea425c06e9c67f31ab6185e
Petar Nikolov <petar.v.nikolov@gmail.com>
2026-03-14 01:04:25 +0200

    feat(logger): add structured logging using zap

---
+++ internal/platform/logger/logger.go
@@ -0,0 +1,14 @@
+package logger
+
+import "go.uber.org/zap"
+
+func New() (*zap.Logger, error) {
+
+	logger, err := zap.NewProduction()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return logger, nil
+}
